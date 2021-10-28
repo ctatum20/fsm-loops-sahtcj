b7f07c1891beced85d7485568dab01293f94bd33
ctatum20 <charnicetatum@gmail.com>
2021-10-27 20:34:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fsm-loops-sahtcj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/fsm-loops-sahtcj)
---
+++ index.js
@@ -0,0 +1,79 @@
+// Create an array and solve for the mean with a for loop
+// let randNums = [56, 34, 587, 5, 27];
+// let sum = 0;
+// for (let i = 0; i < randNums.length; i++) {
+//   sum = sum + randNums[i] / 5;
+// }
+// console.log(sum);
+//----------------------------------------------
+// Convert items in array to a string using a for loop
+
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let newStr = '';
+
+// for (let i = 0; i < nums.length; i++) {
+//   newStr += nums[i];
+// }
+// console.log(newStr);
+//-----------------------------------------------
+//Reverse an array of numbers
+
+// let numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let reversed = [];
+
+// for (let i = numArr.length - 1; i >= 0; i--) {
+//   reversed.push(numArr[i]);
+// }
+// console.log(reversed);
+// ASK LANCE THE WHY BEHIND THE SET UP
+//------------------------------------------------
+
+//Merge two arrays using for loop
+
+let array1 = ['pasta', 'salad', 'noodle soup'];
+let array2 = ['dumplings', 'sushi', 'steak'];
+
+for (let i = 0; i < array2.length; i++) {
+  array1.push(array2[i]);
+}
+console.log(array1);
+//--------------------------------------------------
+//Make an array with strings homework
+
+// let shapes = ['triangle', 'square', 'rectangle', 'circle', 'oval', 'diamond'];
+// let arrShapes = [];
+
+// for (let i = 0; i < shapes.length; i++) {
+//   arrShapes.push(shapes[i]);
+// }
+// console.log(arrShapes);
+//---------------------------------------------------
+//Flatten the array
+
+// let nums = [
+//   [11, 2, 328],
+//   [4, 15, 61],
+//   [17, 8, 19],
+// ];
+
+// let flat = [];
+
+// for (let i = 0; i < nums.length; i++) {
+//   for (let j = 0; j < nums.length; j++) {
+//     flat.push(nums[i][j]); //this pushes both together or "flattens it"
+//   }
+// }
+// console.log(flat);
+//---------------------------------------------
+// let numbers = [1, 2, 3, 4, 5];
+// let 
+// while (start <= end) {
+//   startNumber = numbers[start];
+//   numbers[start] = numbers[end];
+//   numbers[end] = startNumber;
+//   start += 1;
+//   end -= 1;
+// }
+// console.log(numbers);
+
+//array homework: two for loops and a push or create an array and replace the numbers with the nulls nums= [2][0]
